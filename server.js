6d253b6ef329973af37dd5e2b864ab8fc6da43af
Zlandars <mag20102009@gmail.com>
2023-01-09 23:02:19 +0300

    feat: готовая домашка

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
-const http = require('http');
+import http from 'http';
+import {readdir} from 'fs';
+
 const host = 'localhost';
 const port = 3000;
 
-const requestListener = (req,res) => {
-    res.writeHead(200);
-    res.end('Server is started!')
+const requestListener =  (req,res) => {
+    if (req.url === '/get') {
+        if (req.method === 'GET') {// Не прочитал задание все и сразу, поэтому без синхронного метода обошелся...
+            readdir('./files', (err, data) => {
+                if (err) {
+                    res.writeHead(500);
+                    res.end('Internal server error');
+                    return;
+                }
+                res.writeHead(200);
+                res.end(data.join(',\n'))
+            });
+        }
+        res.writeHead(405)
+        res.end('HTTP method not allowed')
+        return;
+    }
+    if (req.url === '/delete') {
+        console.log(req.method)
+        if (req.method === 'DELETE') {
+            res.writeHead(200);
+            res.end('Success! Delete page!')
+            return;
+        }
+        res.writeHead(405)
+        res.end('HTTP method not allowed')
+        return;
+    }
+    if (req.url === '/post') {
+        console.log(req.method)
+        if (req.method === 'POST') {
+            res.writeHead(200);
+            res.end('Success! Post page!')
+            return;
+        }
+        res.writeHead(405)
+        res.end('HTTP method not allowed')
+        return;
+    }
+
+    if (req.url === '/redirect') {
+        if (req.method === 'GET') {
+            res.writeHead(301)
+            res.end('Ресурс постоянно доступен по адресу /redirected')
+        }
+    }
 }
 
 const server = http.createServer(requestListener);
